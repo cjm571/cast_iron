7743741f539fd429dbe76da39d891aeb5416e942
CJ McAllister <cjm571@gmail.com>
2017-11-22 19:44:36 +0000

    Initial commit!

---
+++ src/actor.rs
@@ -0,0 +1,8 @@
+// Filename : actor.rs
+// Author   : CJ McAllister
+// Created  : 22-11-2017
+// License  : TODO: Add license info
+
+pub fn test_fn() {
+    println!("Hello, Actor!");
+}
---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+pub mod actor;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        super::actor::test_fn();
+    }
+}
